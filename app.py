863ef34f30c2fd0ceea1a92522be0eb77980c077
SameC137 <samemichael1415@gmail.com>
2021-07-31 17:21:52 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import pandas_profiling
+import streamlit as st
+
+# from streamlit_gallery.utils import readme
+from streamlit_pandas_profiling import st_profile_report
+
+
+def main():
+    dataset = "train.csv"
+
+    df = pd.read_csv(dataset)
+    pr = gen_profile_report(df.iloc[:10000], explorative=True)
+
+    st.write(f"🔗 [Rossman Sales]({dataset})")
+    st.write(df)
+
+    with st.beta_expander("REPORT", expanded=True):
+        st_profile_report(pr)
+    pr.to_file('train.html')
+
+
+@st.cache(allow_output_mutation=True)
+def gen_profile_report(df, *report_args, **report_kwargs):
+    return df.profile_report(*report_args, **report_kwargs)
+
+
+if __name__ == "__main__":
+    main()
